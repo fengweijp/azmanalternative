c3539bd273bd563b59c6381441ac2f24d563cf0e
simonhalsey <simonhalsey@1ee07a59-b5bd-031a-0137-cb784ff360e6>
2010-11-20 17:22:13 +0000

    Added AD classes

---
+++ trunk/ActiveDirectory/AdBaseObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AzAlternative.ActiveDirectory
+{
+    internal class AdBaseObject
+    {
+        protected const string GUID = "Guid";
+        protected const string NAME = "Name";
+        protected const string DESCRIPTION = "Description";
+
+        public Guid Guid
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public AdBaseObject(AdService service)
+        {
+        }
+    }
+}
